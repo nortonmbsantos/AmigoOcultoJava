53a09657886a6c8cb16b5eaeb8d8f1d91fac957f
ls <nortonmbsantos1@gmail.com>
2019-05-02 12:13:46 -0300

    Implementing main with menu

--- src/amigooculto/AmigoOculto.java
+++ src/amigooculto/AmigoOculto.java
@@ -3,36 +3,56 @@ package amigooculto;
 import amigooculto.models.Amigo;
 import amigooculto.models.Sorteio;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
  * @author norto
  */
 public class AmigoOculto {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Amigo a1 = new Amigo("João");
-        Amigo a2 = new Amigo("Norton");
-        Amigo a3 = new Amigo("Ursula");
-        Amigo a4 = new Amigo("Estevan");
-        Amigo a5 = new Amigo("Livio");
 
-        ArrayList<Amigo> amigos = new ArrayList<>();
-        amigos.add(a1);
-        amigos.add(a2);
-        amigos.add(a3);
-        amigos.add(a4);
-        amigos.add(a5);
-        
-        Sorteio sorteio = new Sorteio(amigos);
-        sorteio.sorteia();
-        sorteio.imprime();
+        menu();
+    }
+    
+    private static ArrayList<Amigo> amigos = new ArrayList<>();
+    
+    public static void menu(){
+        System.out.println("Escolhe entre as opções: ");
+        System.out.println("1 - Adicionar amigo");
+        System.out.println("2 - Sortear");
+        System.out.println("3 - Sair");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Opção: ");
+        int opcao = scanner.nextInt();
+        switch(opcao){
+            case 1:
+                System.out.print("Nome: ");
+                scanner.nextLine();   
+                String nome = scanner.nextLine();
+                adicionarAmigo(nome);
+                break;
+            case 2: 
+                sorteia();
+                break;
+            case 3:
+                break;
+        }
         
     }
     
-    
-   
+    public static void adicionarAmigo(String nome){
+        amigos.add(new Amigo(nome));
+        menu();
+    }
+
+    private static void sorteia() {
+        Sorteio sorteio = new Sorteio(amigos);
+        sorteio.sorteia();
+        sorteio.imprime();
+        menu();
+    }
 }
